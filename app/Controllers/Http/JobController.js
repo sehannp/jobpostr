e480acd96d56f8b4a464e6d92c009f4d6be06d1b
sehannp <sehanp@gmail.com>
2020-02-11 23:10:23 +1100

    get route added

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -3,6 +3,17 @@
 const Job = use('App/Models/Job')
 
 class JobController {
+    async home({view}){
+        // const job = new Job;
+        // job.title = 'My Job Title';
+        // job.link = 'http://google.com';
+        // job.description = 'My Job Desc';
+        // await job.save();
+
+        //fetch
+        const jobs = await Job.all();
+        return view.render('index',{jobs: jobs.toJSON()})
+    }
 }
 
 module.exports = JobController
